d1da27e758ce43c19cdb5c4fbd92d886e149046c
arifthalhah <65509122+arifth@users.noreply.github.com>
2023-05-03 14:06:37 +0700

    Update Doughnut.jsx

--- src/components/Doughnut.jsx
+++ src/components/Doughnut.jsx
@@ -4,18 +4,18 @@ import { Pie } from "react-chartjs-2";
 import { Box } from "@chakra-ui/react";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-// export const data = {
-//   labels: ["women", "Men"],
-//   datasets: [
-//     {
-//       label: "# of Votes",
-//       data: [12, 19],
-//       backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
-//       borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
-//       borderWidth: 1,
-//     },
-//   ],
-// };
+export const data = {
+  labels: ["women", "Men"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19],
+      backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+      borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+      borderWidth: 1,
+    },
+  ],
+};
 
 export function Doughnut({ data }) {
   return (
